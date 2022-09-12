70b54b5eef8f7fccfee687db86a563d2fd40c802
ThomasSanchezS <thomas.sanchezs@utadeo.edu.co>
2022-09-12 12:16:59 -0500

    Movimiento Y Camara 9/12/2022

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,35 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
-
 public class CameraMovement : MonoBehaviour
 {
-    public float mouseX;
-    public float mouseY;
-    public Transform Camerarotation;
-    public float speedRotation = 10f;
-    public float xRotation;
-    
+    public Camera camara;
+    public float horizontalSpeed;
+    public float verticalSpeed;
+    private Transform myTransform;
+    public float speed;
+    private float xAxis, zAxis;
+
+    float h;
+    float v;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        myTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X");
-        float mouseY = Input.GetAxis("Mouse Y");  
+        h = horizontalSpeed*Input.GetAxis("Mouse X");
+        v = verticalSpeed*Input.GetAxis("Mouse Y");
 
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-       
-        Camerarotation.Rotate(Vector3.up * mouseX*speedRotation);
+        transform.Rotate(0,h,0);
+        camara.transform.Rotate(-v,0,0);
         
-        
+        xAxis = Input.GetAxis("Horizontal");
+        zAxis = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(xAxis, 0, zAxis);
+        myTransform.transform.Translate(movimiento*speed*Time.deltaTime);
+
     }
 }
