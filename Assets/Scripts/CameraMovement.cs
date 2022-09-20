729be823f825ca1d906185ba457f522b1a8569b5
ThomasSanchezS <thomas.sanchezs@utadeo.edu.co>
2022-09-20 10:08:19 -0500

    CambioScriptControl 9/20/2022

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -2,38 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
+
 public class CameraMovement : MonoBehaviour
 {
-    public Camera camara;
-    public float horizontalSpeed;
-    public float verticalSpeed;
-    private Transform myTransform;
-    public float speed;
-    private float xAxis, zAxis;
+    public float walkingSpeed = 7.5f;
+    public float runningSpeed = 11.5f;
+    public float jumpSpeed = 8.0f;
+    public float gravity = 20.0f;
+    public Camera playerCamera;
+    public float lookSpeed = 2.0f;
+    public float lookXLimit = 45.0f;
 
-    float h;
-    float v;
+    CharacterController characterController;
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+
+    [HideInInspector]
+    public bool canMove = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        myTransform = GetComponent<Transform>();
+        characterController = GetComponent<CharacterController>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        h = horizontalSpeed*Input.GetAxis("Mouse X");
-        v = verticalSpeed*Input.GetAxis("Mouse Y");
+      
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
 
-        transform.Rotate(0,h,0);
-        camara.transform.Rotate(-v,0,0);
-        
-        xAxis = Input.GetAxis("Horizontal");
-        zAxis = Input.GetAxis("Vertical");
+        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical"): 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal"): 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
 
-        Vector3 movimiento = new Vector3(xAxis, 0, zAxis);
-        myTransform.transform.Translate(movimiento*speed*Time.deltaTime);
+        if(Input.GetButton("Jump") && canMove && characterController.isGrounded){
+            moveDirection.y = jumpSpeed;
+        } else{
+            moveDirection.y = movementDirectionY;
+        }
 
+        if(!characterController.isGrounded){
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        if(canMove){
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
     }
 }
